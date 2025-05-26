dffb3d59d0b3bbcd0a6ff7401cfcfc01cf85dcc9
planck <shashvat817@gmail.com>
2025-05-26 16:44:28 +0530

    init

---
+++ prettier.config.js
@@ -0,0 +1,25 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+    tabWidth: 4,
+    printWidth: 100,
+    importOrder: [
+        "^(react/(.*)$)|^(react$)",
+        "^(hono/(.*)$)|^(hono$)",
+        "^(drizzle-orm/(.*)$)|^(drizzle-orm$)",
+        "^(@tanstack/react-router/(.*)$)|^(@tanstack/react-router$)",
+        "^(@tanstack/(.*)$)|^(@tanstack$)",
+        "",
+        "<THIRD_PARTY_MODULES>",
+        "",
+        "^types$",
+        "^@/shared/(.*)$",
+        "^@/lib/(.*)$",
+        "^@/hooks/(.*)$",
+        "^@/components/ui/(.*)$",
+        "^@/components/(.*)$",
+        "",
+        "^[./]",
+    ],
+    importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+    plugins: ["@ianvs/prettier-plugin-sort-imports"],
+};
