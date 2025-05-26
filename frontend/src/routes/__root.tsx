a6010e3d5cfd9b09d9f27744450b448926acf237
planck <shashvat817@gmail.com>
2025-05-26 16:44:28 +0530

    setup frontend tanstack-router/query + tailwindcss

---
+++ frontend/src/routes/__root.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRoute({
+    component: RootComponent,
+});
+
+function RootComponent() {
+    return (
+        <>
+            <div className="p-2 flex gap-2 text-lg">
+                <Link
+                    to="/"
+                    activeProps={{
+                        className: "font-bold",
+                    }}
+                    activeOptions={{ exact: true }}
+                >
+                    Home
+                </Link>{" "}
+                <Link
+                    to="/about"
+                    activeProps={{
+                        className: "font-bold",
+                    }}
+                >
+                    About
+                </Link>
+            </div>
+            <hr />
+            <Outlet />
+            <ReactQueryDevtools />
+            <TanStackRouterDevtools position="bottom-left" />
+        </>
+    );
+}
